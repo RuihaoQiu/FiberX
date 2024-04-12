e640b940b9580ac553b70173c86e4371221a561e
Ruihao Qiu <qiuruihao@gmail.com>
2024-04-12 11:11:19 +0800

    complete front end design

--- app.py
+++ app.py
@@ -47,55 +47,6 @@ class App(ttk.Frame):
     def setup_plots(self):
         self.build_plot_block()
 
-    def setup_tab1(self):
-        self.tab1 = ttk.Frame(self.notebook)
-        # self.tab1.columnconfigure(index=0, weight=1)
-        # self.tab1.columnconfigure(index=1, weight=1)
-        self.notebook.add(self.tab1, text="光谱")
-
-        self.build_buttons_block()
-        # self.build_dark_block()
-        # self.build_ref_block()
-        self.build_plot_block()
-
-    def build_buttons_block(self):
-        # Create a Frame for buttons
-        self.buttons_frame = ttk.LabelFrame(self, padding=(0, 0, 0, 10))
-        self.buttons_frame.grid(row=0, column=1, padx=10, pady=(30, 10), sticky="nsew")
-        self.buttons_frame.columnconfigure(index=0, weight=1)
-
-        self.start_button = ttk.Button(
-            self.buttons_frame,
-            text="开始",
-            command=self.start_real,
-            style="Accent.TButton",
-        )
-        self.start_button.grid(row=0, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.stop_button = ttk.Button(
-            self.buttons_frame,
-            text="结束",
-            command=self.stop_real,
-            style="Accent.TButton",
-        )
-        self.stop_button.grid(row=1, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.save_dark_button = ttk.Button(
-            self.buttons_frame,
-            text="保存暗光谱",
-            command=self.save_dark,
-            style="Accent.TButton",
-        )
-        self.save_dark_button.grid(row=2, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.save_ref_button = ttk.Button(
-            self.buttons_frame,
-            text="保存亮光谱",
-            command=self.save_ref,
-            style="Accent.TButton",
-        )
-        self.save_ref_button.grid(row=3, column=0, padx=5, pady=(0, 10), sticky="ew")
-
     def build_dark_block(self):
         dark_frame = ttk.LabelFrame(self, text="暗光谱", padding=(20, 10))
         dark_frame.grid(
@@ -175,11 +126,9 @@ class App(ttk.Frame):
             pady=(10, 10),
             sticky="nsew",
         )
-        # Create a label
+
         label = ttk.Label(input_frame, text="Enter your input:")
         label.grid(row=0, column=0, padx=30, pady=(0, 10), sticky="ew")
-
-        # Create the entry widget
         entry = ttk.Entry(input_frame)
         entry.grid(row=1, column=0, padx=30, pady=(0, 10), sticky="ew")
 
@@ -225,22 +174,30 @@ class App(ttk.Frame):
         save_button.grid(row=2, column=0, padx=30, pady=(0, 10), sticky="ew")
 
     def build_plot_block(self):
-        # Panedwindow
         self.paned = ttk.PanedWindow(self)
-        self.paned.grid(row=0, column=2, pady=(25, 5), sticky="nsew", rowspan=3)
-
-        # Pane #1
-        # self.pane_1 = ttk.Frame(self, padding=100)
-        # self.paned.add(self.pane_1, weight=3)
+        self.paned.grid(
+            row=0,
+            column=2,
+            padx=(25, 10),
+            pady=(25, 10),
+            sticky="nsew",
+            rowspan=3,
+        )
 
         self.notebook = ttk.Notebook(self.paned)
         self.notebook.pack(fill="both", expand=True)
 
-        self.tab1 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab1, text="光谱")
+        self.build_tab1()
+        self.build_tab2()
+        self.build_tab3()
+        self.build_tab4()
 
-        self.plot_frame = ttk.Frame(self.tab1)
-        self.plot_frame.grid(
+    def build_tab1(self):
+        tab1 = ttk.Frame(self.notebook)
+        self.notebook.add(tab1, text="光谱")
+
+        plot_frame = ttk.Frame(tab1)
+        plot_frame.grid(
             row=0,
             column=0,
             padx=(150, 150),
@@ -250,19 +207,100 @@ class App(ttk.Frame):
             columnspan=3,
         )
 
-        self.fig1, self.ax1 = plt.subplots()
+        fig1, self.ax1 = plt.subplots()
         self.ax1.set_xlabel("Wavelength")
         self.ax1.set_ylabel("Intensity")
 
-        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.plot_frame)
-        self.canvas1.draw()
-        self.canvas1.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        canvas = FigureCanvasTkAgg(fig1, master=plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-        (self.dark,) = self.ax1.plot([], [], "-", label="Dark")
-        (self.reference,) = self.ax1.plot([], [], "-", label="Reference")
-        (self.realtime,) = self.ax1.plot([], [], "-", label="Real time")
+        (dark,) = self.ax1.plot([], [], "-", label="Dark")
+        (reference,) = self.ax1.plot([], [], "-", label="Reference")
+        (realtime,) = self.ax1.plot([], [], "-", label="Real time")
 
         self.ax1.legend()
+
+    def build_tab2(self):
+        tab2 = ttk.Frame(self.notebook)
+        self.notebook.add(tab2, text="透射")
+
+        plot_frame = ttk.Frame(tab2)
+        plot_frame.grid(
+            row=0,
+            column=0,
+            padx=(150, 150),
+            pady=(200, 10),
+            sticky="ew",
+            rowspan=3,
+            columnspan=3,
+        )
+
+        fig2, self.ax2 = plt.subplots()
+        self.ax2.set_xlabel("Wavelength")
+        self.ax2.set_ylabel("Y")
+
+        canvas = FigureCanvasTkAgg(fig2, master=plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+        (realtime,) = self.ax2.plot([], [], "-", label="Real time")
+
+        self.ax2.legend()
+
+    def build_tab3(self):
+        tab3 = ttk.Frame(self.notebook)
+        self.notebook.add(tab3, text="时序")
+
+        plot_frame = ttk.Frame(tab3)
+        plot_frame.grid(
+            row=0,
+            column=0,
+            padx=(150, 150),
+            pady=(200, 10),
+            sticky="ew",
+            rowspan=3,
+            columnspan=3,
+        )
+
+        fig3, self.ax3 = plt.subplots()
+        self.ax3.set_xlabel("Time")
+        self.ax3.set_ylabel("Y")
+
+        canvas = FigureCanvasTkAgg(fig3, master=plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+        (realtime,) = self.ax3.plot([], [], "-", label="Real time")
+
+        self.ax3.legend()
+
+    def build_tab4(self):
+        tab4 = ttk.Frame(self.notebook)
+        self.notebook.add(tab4, text="强度时序")
+
+        plot_frame = ttk.Frame(tab4)
+        plot_frame.grid(
+            row=0,
+            column=0,
+            padx=(150, 150),
+            pady=(200, 10),
+            sticky="ew",
+            rowspan=3,
+            columnspan=3,
+        )
+
+        fig4, self.ax4 = plt.subplots()
+        self.ax4.set_xlabel("Time")
+        self.ax4.set_ylabel("Y")
+
+        canvas = FigureCanvasTkAgg(fig4, master=plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+        (realtime,) = self.ax4.plot([], [], "-", label="Real time")
+
+        self.ax4.legend()
 
     def load_dark(self, file_path):
         x, y = load_file(self.dark_folder + file_path.get())
@@ -295,126 +333,6 @@ class App(ttk.Frame):
 
     def stop_real(self):
         self.running = False
-
-    def setup_tab2(self):
-        self.tab2 = ttk.Frame(self.notebook)
-        self.tab2.columnconfigure(index=0, weight=1)
-        self.tab2.columnconfigure(index=1, weight=1)
-        self.notebook.add(self.tab2, text="吸收谱")
-
-        self.build_buttons_block2()
-        # self.build_dark_block2()
-        # self.build_ref_block2()
-        # self.build_plot_block2()
-
-    def build_buttons_block2(self):
-        # Create a Frame for buttons
-        self.buttons_frame2 = ttk.Frame(self.tab2, padding=(0, 0, 0, 10))
-        self.buttons_frame2.grid(row=0, column=0, padx=10, pady=(30, 10), sticky="nsew")
-        self.buttons_frame2.columnconfigure(index=0, weight=2)
-
-        self.start_button2 = ttk.Button(
-            self.buttons_frame2,
-            text="开始",
-            command=self.start_real,
-            style="Accent.TButton",
-        )
-        self.start_button2.grid(row=0, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.stop_button2 = ttk.Button(
-            self.buttons_frame2,
-            text="结束",
-            command=self.stop_real,
-            style="Accent.TButton",
-        )
-        self.stop_button2.grid(row=1, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.save_dark_button2 = ttk.Button(
-            self.buttons_frame2,
-            text="保存暗光谱",
-            command=self.save_dark,
-            style="Accent.TButton",
-        )
-        self.save_dark_button2.grid(row=2, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-        self.save_ref_button2 = ttk.Button(
-            self.buttons_frame2,
-            text="保存亮光谱",
-            command=self.save_ref,
-            style="Accent.TButton",
-        )
-        self.save_ref_button2.grid(row=3, column=0, padx=5, pady=(0, 10), sticky="ew")
-
-    def build_dark_block2(self):
-        dark_frame2 = ttk.LabelFrame(self.tab2, text="暗光谱", padding=(20, 10))
-        dark_frame2.grid(
-            row=1,
-            column=0,
-            padx=(10, 10),
-            pady=(10, 10),
-            sticky="nsew",
-        )
-
-        var = tk.StringVar()
-        filenames = os.listdir(self.dark_folder)
-        for file in filenames:
-            b = ttk.Checkbutton(
-                dark_frame2,
-                text=file,
-                variable=var,
-                onvalue=file,
-                command=lambda var=var: self.load_dark(var),
-            )
-            b.pack(anchor=tk.W)
-
-    def build_ref_block2(self):
-        ref_frame2 = ttk.LabelFrame(self.tab2, text="参考光谱", padding=(20, 10))
-        ref_frame2.grid(
-            row=2,
-            column=0,
-            padx=(10, 10),
-            pady=(10, 10),
-            sticky="nsew",
-        )
-
-        var = tk.StringVar()
-        filenames = os.listdir(self.ref_folder)
-        for file in filenames:
-            b = ttk.Checkbutton(
-                ref_frame2,
-                text=file,
-                variable=var,
-                onvalue=file,
-                command=lambda var=var: self.load_ref(var),
-            )
-            b.pack(anchor=tk.W)
-
-    def build_plot_block2(self):
-        self.plot_frame2 = ttk.Frame(self.tab2)
-        self.plot_frame2.columnconfigure(index=1, weight=1)
-        self.plot_frame2.grid(
-            row=0,
-            column=1,
-            padx=(10, 10),
-            pady=(10, 10),
-            sticky="nsew",
-            rowspan=3,
-            columnspan=3,
-        )
-
-        self.fig2, self.ax2 = plt.subplots()
-        self.ax2.set_xlabel("Wavelength")
-        self.ax2.set_ylabel("Intensity")
-
-        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.plot_frame2)
-        self.canvas2.draw()
-        self.canvas2.get_tk_widget().grid(
-            row=0, column=1, columnspan=3, rowspan=3, padx=10, pady=10, sticky="nsew"
-        )  # .pack(fill=tk.BOTH, expand=True)
-
-        (self.realtime,) = self.ax2.plot([], [], "-", label="Real time")
-
-        self.ax2.legend()
 
     def save_absorb(self):
         pass
